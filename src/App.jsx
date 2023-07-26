662d129c3fcbc8f048c3619608f5b09dbb23b929
Dalibor Petrovic <daliborpskopje@gmail.com>
2023-07-26 11:03:22 +0200

    added initial theme configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "@emotion/react";
+import { Button, Typography } from "@mui/material";
+
+import Theme from "./theme/Theme";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ThemeProvider theme={Theme}>
+      <Typography variant="h2">IWParking h2</Typography>
+      <Typography variant="h3">IWParking h3</Typography>
+      <Typography paragraph>IWParking</Typography>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="primary"
+        >
+          Primary
+        </Button>
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="secondary"
+        >
+          Secondary
+        </Button>
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="error"
+        >
+          Error
+        </Button>
+        <br />
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="warning"
+        >
+          Warning
+        </Button>
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="info"
+        >
+          Info
+        </Button>
+        <Button
+          sx={{ width: "200px", margin: "10px" }}
+          variant="contained"
+          color="success"
+        >
+          Success
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
